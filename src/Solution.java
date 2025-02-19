258d9ed1c51adfa83c4ba2c86d99fb8dddacf9e6
Maoka2 <a24358232@gmail.com>
2025-02-19 14:53:45 +0900

    SWEA 5110 수열 뒤집기 LinkedList 사용

--- src/Solution.java
+++ src/Solution.java
@@ -7,36 +7,51 @@ public class Solution {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
-        for(int tc = 1; tc <=T; tc++){
-
-            int N = Integer.parseInt(br.readLine());
-            int[] buttons = new int[N];
-            int[] result = new int[N];
-            String[] a = br.readLine().split(" ");
-            String[] b = br.readLine().split(" ");
-
-            for(int i = 0; i < N; i++){
-
-                buttons[i] = Integer.parseInt(a[i]);
-                result[i] = Integer.parseInt(b[i]);
-
-            }
-            int count = 0;
-
-            for(int i = 0; i < N; i++){
-                if(buttons[i] != result[i]){
-                    count++;
-                    for(int j = i; j < N; j++){
-                        buttons[j] = 1-buttons[j];
+        for (int tc = 1; tc <= T; tc++) {
+            String[] s = br.readLine().split(" ");
+            int N = Integer.parseInt(s[0]);
+            int M = Integer.parseInt(s[1]);
+            LinkedList<Integer> ll = new LinkedList<>();
+            int[][] nums = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                String[] ss = br.readLine().split(" ");
+                for (int j = 0; j < M; j++) {
+                    nums[i][j] = Integer.parseInt(ss[j]);
+                    if (i == 0) {
+                        ll.add(Integer.parseInt(ss[j]));
                     }
                 }
             }
-            bw.write("#" + tc + " " + count + "\n");
+
+
+            for (int i = 0; i < N - 1; i++) {
+                int index = -1;
+                for (int j = 0; j < M; j++) {
+                    if (ll.get(j) > nums[i + 1][0]) {
+                        index = j;
+                        break;
+                    }
+                }
+                if (index == -1) {
+                    for (int k = 0; k < M; k++) {
+                        ll.addLast(nums[i + 1][k]);
+                    }
+                } else {
+                    for (int k = 0; k < M; k++) {
+                        ll.add(index, nums[i + 1][k]);
+                        index++;
+                    }
+                }
+            }
+
+            bw.write("#" + tc + " ");
+            for (int i = 0; i < 10; i++) {
+                bw.write(ll.pollLast() + " ");
+            }
+            bw.write("\n");
+
 
         }
-
-
-
         bw.flush();
         br.close();
         bw.close();
