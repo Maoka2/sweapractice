a700d5519104149589a76704dbb9ecafd51e9672
Maoka2 <a24358232@gmail.com>
2025-02-19 15:04:58 +0900

    SWEA 5108 숫자합치기 LinkedList 사용

--- src/Solution.java
+++ src/Solution.java
@@ -11,46 +11,26 @@ public class Solution {
             String[] s = br.readLine().split(" ");
             int N = Integer.parseInt(s[0]);
             int M = Integer.parseInt(s[1]);
+            int L = Integer.parseInt(s[2]);
+
+            int[] num = new int[N];
+            String[] sssss = br.readLine().split(" ");
+
+            for(int i = 0; i < N; i++){
+                num[i] = Integer.parseInt(sssss[i]);
+            }
+
             LinkedList<Integer> ll = new LinkedList<>();
-            int[][] nums = new int[N][M];
             for (int i = 0; i < N; i++) {
+                ll.add(num[i]);
+            }
+
+            for (int i = 0; i < M; i++) {
                 String[] ss = br.readLine().split(" ");
-                for (int j = 0; j < M; j++) {
-                    nums[i][j] = Integer.parseInt(ss[j]);
-                    if (i == 0) {
-                        ll.add(Integer.parseInt(ss[j]));
-                    }
-                }
+
+                ll.add(Integer.parseInt(ss[0]), Integer.parseInt(ss[1]));
             }
-
-
-            for (int i = 0; i < N - 1; i++) {
-                int index = -1;
-                for (int j = 0; j < M; j++) {
-                    if (ll.get(j) > nums[i + 1][0]) {
-                        index = j;
-                        break;
-                    }
-                }
-                if (index == -1) {
-                    for (int k = 0; k < M; k++) {
-                        ll.addLast(nums[i + 1][k]);
-                    }
-                } else {
-                    for (int k = 0; k < M; k++) {
-                        ll.add(index, nums[i + 1][k]);
-                        index++;
-                    }
-                }
-            }
-
-            bw.write("#" + tc + " ");
-            for (int i = 0; i < 10; i++) {
-                bw.write(ll.pollLast() + " ");
-            }
-            bw.write("\n");
-
-
+            bw.write("#" + tc + " " + ll.get(L) + "\n");
         }
         bw.flush();
         br.close();
