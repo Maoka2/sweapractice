44e5398783b3e6a297c81b7eba33ab3c0382f99f
Maoka2 <a24358232@gmail.com>
2025-02-20 23:06:03 +0900

    SWEA 1225 암호생성기
    덱을 이용한 데이터 조작

--- src/Solution.java
+++ src/Solution.java
@@ -5,36 +5,45 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int T = 10;
         for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
             String[] s = br.readLine().split(" ");
-            int N = Integer.parseInt(s[0]);
-            int M = Integer.parseInt(s[1]);
-            Map<Integer, Integer> cardgame = new HashMap<>();
+            Deque<Integer> pw = new ArrayDeque<>();
+            for(int i = 0; i < s.length; i++){
+                pw.add(Integer.parseInt(s[i]));
+            }
+            sb.append("#").append(tc).append(" ");
 
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= M; j++) {
-                    cardgame.put(i + j, cardgame.getOrDefault(i + j, 0) + 1);
+            int count = 0;
+            int minus = 1;
+
+
+            while(true){
+                if(pw.peekFirst() - minus <= 0){
+                    pw.pollFirst();
+                    pw.addLast(0);
+                    break;
                 }
-            }
-
-            int max = -1;
-            for (int n : cardgame.keySet()) {
-                max = Math.max(max, cardgame.get(n));
-            }
-
-            bw.write("#" + tc + " ");
-            for(int n : cardgame.keySet()){
-                if(cardgame.get(n) == max){
-                    bw.write(n + " ");
+                pw.addLast(pw.pollFirst() - minus);
+                minus++;
+                if(minus == 6){
+                    minus = 1;
                 }
+
             }
-            bw.write("\n");
+            for(int n : pw){
+                sb.append(pw.pollFirst()).append(" ");
+            }
+            String ans = sb.toString().trim();
+            bw.write(ans + "\n");
+            sb.setLength(0);
         }
 
 
         bw.flush();
         br.close();
         bw.close();
-    }
+}
 }
